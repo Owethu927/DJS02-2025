b5a2934bfda0f04976e7bf163aed637e05e69ed9
Owethu927 <owethusilela@gmail.com>
2025-09-25 14:18:01 +0300

    created a template the will store our inner html and appended it to our class

--- podcastPreview.js
+++ podcastPreview.js
@@ -1,7 +1,17 @@
+//Create a template
+const template = document.createElement("template");
+template.innerHTML = `
+ <style>
+ 
+ <style>`;
+
 // Creating a class
 class PodcastPreview extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
+
+window.customElements.define("podcast-preview", PodcastPreview);
