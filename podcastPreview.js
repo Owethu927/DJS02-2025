9f7b91fd72800fa59311eb5d9c5a82359a41bda8
Owethu927 <owethusilela@gmail.com>
2025-09-25 15:10:35 +0300

    added styling and inner html

--- podcastPreview.js
+++ podcastPreview.js
@@ -2,8 +2,64 @@
 const template = document.createElement("template");
 template.innerHTML = `
  <style>
+
+
+.card {
+  background: white;
+  padding: 1rem;
+  border-radius: 8px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+
+.card:hover {
+  transform: scale(1.02);
+}
+
+.card img {
+  width: 100%;
+  border-radius: 6px;
+}
+
+.card h3 {
+  margin: 0.5rem 0;
+}
+
+.card p {
+  margin: 0px;
+  font-size: 0.8rem;
+  color: var(--grey-text);
+}
+
+.tags {
+  margin: 0.5rem 0;
+}
+
+.tag {
+  background: #eee;
+  padding: 0.3rem 0.6rem;
+  margin-right: 0.5rem;
+  margin-top: 0.5rem;
+  border-radius: 4px;
+  display: inline-block;
+  font-size: 0.8rem;
+}
+
+.updated-text {
+  font-size: 0.8rem;
+  color: var(--grey-text);
+}
+
  
- <style>`;
+ <style>
+ 
+ <div class="card">
+  <img />
+  <h3></h3>
+  <div class"tag"></div>
+  <div class"updated-text"></div>
+ <div>`;
 
 // Creating a class
 class PodcastPreview extends HTMLElement {
